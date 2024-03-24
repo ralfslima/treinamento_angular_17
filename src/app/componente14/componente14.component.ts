18ff88b4d5a1064a9754f34588f93f41e70e555d
ralflima <ralf.lima@gmail.com>
2024-03-24 18:06:26 -0300

    Enviando projeto

---
+++ src/app/componente14/componente14.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-componente14',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './componente14.component.html',
+  styleUrl: './componente14.component.css'
+})
+export class Componente14Component {
+
+}
